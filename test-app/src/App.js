bf9aaf359a74579bd5a1572abc26b04268a9b160
Dmitri <dmitriofishion@gmail.com>
2025-06-23 16:47:56 +0300

    DZ-39

---
+++ test-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Button from './button.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <Button>FIND OUT MORE</Button>
+    </div>
+  );
+}
---
+++ test-app/src/button.jsx
@@ -0,0 +1,11 @@
+import "./button.css";
+
+export const Button = ({ children }) => {
+    return (
+        <button className="button">
+            {children}
+        </button>
+    );
+};
+
+export default Button;
