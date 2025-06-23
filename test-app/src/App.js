6d843e7ced7e64a5d3f7bd4c5a515b92abbbd791
Dmitri <dmitriofishion@gmail.com>
2025-06-23 17:02:58 +0300

    DZ-39

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Button from './button.jsx';
+import Card from './card.jsx';
 
 export default function App() {
   return (
     <div>
       <Button>FIND OUT MORE</Button>
+      <div className="app">
+        <Card
+          title="– ROBO –"
+          price="15.000 ₽"
+          description="УМК по робототехнике"
+          buttonText="Оставить заявку"
+        />
+        <Card
+          title="– ROBO –"
+          price="10.000 ₽"
+          description="УМК по программированию"
+          buttonText="Оставить заявку"
+        />
+      </div>
     </div>
+
   );
 }
+
+
